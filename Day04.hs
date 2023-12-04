7dc688e60603f7dc0ca87c9d7cbba43bb9dfb897
Laurent Bossavit <laurent.git@bossavit.com>
2023-12-04 08:48:03 +0100

    Day 4 part 2

--- Day04.hs
+++ Day04.hs
@@ -1,6 +1,6 @@
 import Data.Text (pack, unpack, splitOn)
 import Data.List (intersect)
-type Card = ([Int], [Int])
+type Card = ([Integer], [Integer])
 
 parseCard :: String -> Card
 parseCard s = asPair nums
@@ -9,13 +9,27 @@ parseCard s = asPair nums
           nums = map (map read . words . unpack) leftRight
           asPair [x,y] = (x,y)
 
-value :: Card -> Int
-value (winning, owned) = if winners > 0 then 2 ^ (winners-1) else 0
-    where winners = length (winning `intersect` owned)
+value :: Card -> Integer
+value card = if winners > 0 then 2 ^ (winners-1) else 0
+    where winners = matches card
 
-part1 :: [Card] -> Int
+matches :: Card -> Integer
+matches(winning, owned) = toInteger $ length (winning `intersect` owned)
+
+part1 :: [Card] -> Integer
 part1 = sum . map value
+
+step :: (Int, ([Integer], [Integer])) -> (Int, ([Integer], [Integer]))
+step (card,(matches,counts)) = (card+1, (matches, counts'))
+    where counts' = zipWith (+) counts copies
+          copies = replicate (card+1) 0 ++ replicate (fromInteger $ matches !! card) (counts !! card) ++ repeat 0
+
+part2 :: [Card] -> Integer
+part2 cards = sum $ snd $ snd $ last $ take (count+1) $ iterate step (0,(matched,replicate count 1))
+    where matched = map matches cards
+          count = length cards
 
 main = do
     cards <- map parseCard . lines <$> readFile "day04.txt"
     print $ part1 cards
+    print $ part2 cards
