d3b0566b1a5f510f403564ea3be8b34cc1dea33b
Laurent Bossavit <laurent.git@bossavit.com>
2023-12-09 09:52:54 +0100

    Day 9 part 2

--- Day09.hs
+++ Day09.hs
@@ -10,12 +10,20 @@ extrapolate :: [[Int]] -> [Int]
 extrapolate = foldr1 addLast
     where addLast two one = two ++ [last one + last two]
 
+artxepolate :: [[Int]] -> [Int]
+artxepolate = foldr1 addFirst
+    where addFirst two one = (head two - head one) : two
+
 part1 :: [[Int]] -> Int
 part1 = sum . map (last . extrapolate . derivatives)
+
+part2 :: [[Int]] -> Int
+part2 = sum . map (head . artxepolate . derivatives)
 
 main = do
     readings <- map (map read . words) . lines <$> readFile "day09.txt"
     print $ part1 readings
+    print $ part2 readings
 
 test = hspec $ do
   describe "Derivatives" $ do
@@ -28,3 +36,6 @@ test = hspec $ do
         extrapolate [[0,3,6,9,12,15],[3,3,3,3,3]] `shouldBe` [0,3,6,9,12,15,18]
         extrapolate [[1,3,6,10,15,21],[2,3,4,5,6],[1,1,1,1]] `shouldBe` [1,3,6,10,15,21,28]
         extrapolate (derivatives [10,13,16,21,30,45]) `shouldBe` [10,13,16,21,30,45,68]
+    it "Extrapolate backwards" $ do
+        artxepolate (derivatives [10,13,16,21,30,45]) `shouldBe` [5,10,13,16,21,30,45]
+
