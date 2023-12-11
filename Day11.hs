25b2e0818c1cf61ac810308f4bffa9ab1b638986
Laurent Bossavit <laurent.git@bossavit.com>
2023-12-11 08:28:56 +0100

    Day 11 part 2

--- Day11.hs
+++ Day11.hs
@@ -18,7 +18,17 @@ part1 grid = sum (map dist $ pairs galaxies)
           pairs l = [(one,two) | n <- [0..length l-1], let one = l !! n, n2 <- [(n+1)..length l-1], let two = l !! n2]
           galaxies = M.keys $ M.filter (=='#') grid
 
+expandBy :: Int -> [String] -> Grid
+expandBy expansion universe = M.mapKeys doExp $ asMap universe
+    where vertScale = scale universe
+          horzScale = scale $ transpose universe
+          scale = scanl (\count l -> if '#' `elem` l then count + 1 else count + expansion) 0
+          doExp (x,y) = (horzScale !! x, vertScale !! y)
+
 main = do
-    grid <- asMap . expand . lines <$> readFile "day11.txt"
-    print $ part1 grid
+    universe <- lines <$> readFile "day11.txt"
+    print $ part1 $ asMap $ expand universe
+    print $ part1 $ expandBy 1000000 universe
+
+
 
