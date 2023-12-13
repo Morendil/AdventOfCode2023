e126b24e4d146d6a5d902821ea515c507db39c25
Laurent Bossavit <laurent.git@bossavit.com>
2023-12-13 09:06:01 +0100

    Day 13 part 2

--- Day13.hs
+++ Day13.hs
@@ -1,21 +1,25 @@
 import Data.Text (pack, unpack, splitOn)
 import Data.List (transpose)
 
-reflectsAbout :: Int -> String -> Bool
-reflectsAbout n s = and $ zipWith (==) (reverse $ take n s) (drop n s)
+reflectAbout :: Int -> String -> [Bool]
+reflectAbout n s = zipWith (/=) (reverse $ take n s) (drop n s)
     where l = length s
 
-axes :: [String] -> [Int]
-axes pattern = filter isAxis [1..l-1]
+axes :: Int -> [String] -> [Int]
+axes smudges pattern = filter isAxis [1..l-1]
     where l = length $ head pattern
-          isAxis n = all (reflectsAbout n) pattern
+          isAxis n = smudges == length (filter id $ concatMap (reflectAbout n) pattern)
 
-value :: [String] -> Int
-value pattern = sum (axes pattern) + 100 * sum (axes $ transpose pattern)
+value :: Int -> [String] -> Int
+value smudges pattern = sum (axes smudges pattern) + 100 * sum (axes smudges $ transpose pattern)
 
 part1 :: [[String]] -> Int
-part1 = sum . map value
+part1 = sum . map (value 0)
+
+part2 :: [[String]] -> Int
+part2 = sum . map (value 1)
 
 main = do
     patterns <- map (lines . unpack) . splitOn (pack "\n\n") . pack <$> readFile "day13.txt"
     print $ part1 patterns
+    print $ part2 patterns
