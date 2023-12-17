1121f81293e85bcc9d24ca198164e71c4e313143
Laurent Bossavit <laurent.git@bossavit.com>
2023-12-17 20:33:31 +0100

    Actually faster with just Dijkstra…

--- Day17.hs
+++ Day17.hs
@@ -1,6 +1,6 @@
 import qualified Data.Map as M
 import Data.Char (digitToInt, intToDigit)
-import Algorithm.Search (aStar)
+import Algorithm.Search (dijkstra)
 import Data.Maybe (fromJust)
 
 data Dir = North | East | South | West deriving (Eq, Show, Enum, Ord)
@@ -11,11 +11,19 @@ type Grid = M.Map Pos Int
 asMap :: [String] -> Grid
 asMap lines = M.fromList [((x,y), digitToInt $ lines !! y !! x) | y <- [0..length lines-1], x <- [0..length (head lines)-1]]
 
+add (x1,y1) (x2,y2) = (x1+x2,y1+y2)
+offsets = [(0,-1),(-1,0),(0,1),(1,0)]
+        
 neighbors :: Pos -> State -> [State]
 neighbors (xMax,yMax) ((x,y),(dir,straight)) = filter (inBounds.fst) moves
-  where add (x1,y1) (x2,y2) = (x1+x2,y1+y2)
-        offsets = [(0,-1),(-1,0),(0,1),(1,0)]
-        moves = [(add (x,y) (offsets !! fromEnum d),(d, if d == dir then straight+1 else 0)) | d <- turns, d /= dir || straight < 2]
+  where moves = [(add (x,y) (offsets !! fromEnum d),(d, if d == dir then straight+1 else 1)) | d <- turns, d /= dir || straight < 3]
+        turns = [toEnum ((fromEnum dir +1) `mod` 4), toEnum ((fromEnum dir +3) `mod` 4), dir]
+        inBounds (x,y) = x >= 0 && y >= 0 && x <= xMax && y <= yMax
+
+ultra :: Pos -> State -> [State]
+ultra (xMax,yMax) ((x,y),(dir,straight)) = filter (inBounds.fst) moves
+  where moves = [(add (x,y) (offsets !! fromEnum d),(d, if d == dir then straight+1 else 1)) | d <- turns, good d dir]
+        good nextDir oldDir = (straight == 0) || (nextDir /= oldDir && straight > 3) || (nextDir == oldDir && straight < 10)
         turns = [toEnum ((fromEnum dir +1) `mod` 4), toEnum ((fromEnum dir +3) `mod` 4), dir]
         inBounds (x,y) = x >= 0 && y >= 0 && x <= xMax && y <= yMax
 
@@ -33,15 +41,25 @@ display grid = [ [ rep $ M.lookup (x,y) grid | x <- [0..xMax]] | y <- [0..yMax]]
 debug :: [State] -> Grid -> M.Map Pos Char
 debug path grid = M.union (toMap path) (M.map intToDigit grid)
 
-part1 :: Grid -> Int
-part1 city = fst path
+solve :: (State -> Bool) -> (Pos -> State -> [State]) -> Grid -> (Int, [State])
+solve arrived neighbors city = path
   -- Start at -1 straight line distance because the starting point doesn't count…
-  where start = ((0,0),(South,-1))
+  where start = ((0,0),(South,0))
         goal = (maximum $ map fst $ M.keys city, maximum $ map snd $ M.keys city)
         bounds = goal
         dist (x1,y1) (x2,y2) = abs (x2-x1) + abs (y2-y1)
-        path = fromJust $ aStar (neighbors bounds) (const (fromJust . (`M.lookup` city) . fst)) (dist goal . fst) ((==) goal . fst) start
+        path = fromJust $ dijkstra (neighbors bounds) (const (fromJust . (`M.lookup` city) . fst)) arrived start
+
+part1 :: Grid -> Int
+part1 city = fst $ solve ((==) goal . fst) neighbors city
+    where goal = (maximum $ map fst $ M.keys city, maximum $ map snd $ M.keys city)
+
+part2 :: Grid -> Int
+part2 city = fst $ solve arriveProper ultra city
+    where goal = (maximum $ map fst $ M.keys city, maximum $ map snd $ M.keys city)
+          arriveProper (pos,(_,straight)) = pos == goal && straight >= 4
 
 main = do
-    city <- asMap . lines <$> readFile "day17_sample.txt"
+    city <- asMap . lines <$> readFile "day17.txt"
     print $ part1 city
+    print $ part2 city
